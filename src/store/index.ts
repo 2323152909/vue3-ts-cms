285af70c522dc012082d284f4e551290765add8e
廖浩东 <15123603553@163.com>
2021-10-24 15:32:39 +0800

    feat(readme): new readme

---
+++ src/store/index.ts
@@ -0,0 +1,13 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state: {
+    counter: 0,
+    name: 'coderlhd'
+  },
+  mutations: {},
+  actions: {},
+  getters: {}
+})
+
+export default store
